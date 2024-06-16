aa50fcba8a79cbb84f4d6d52a93fdbd3551652ad
akash-parashar <aparashar508@gmail.com>
2024-06-17 01:56:29 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("welcomt to ebook apis")
+import app from "./src/app"
+
+
+const startServer=()=>{
+    const PORT = process.env.PORT || 3000;
+
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+}
+startServer();
