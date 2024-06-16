3501a32d2f551646fbde02b3251cc90d85f21a91
akash-parashar <aparashar508@gmail.com>
2024-06-17 02:29:28 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import app from "./src/app"
+import { config } from "./src/config/config";
 
 
 const startServer=()=>{
-    const PORT = process.env.PORT || 3000;
+    const PORT = config.port || 3000;
 
 
     app.listen(PORT,()=>{
