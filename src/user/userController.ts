8bb13c901cd4471441cee2353e91cf46a4d3271a
akash-parashar <aparashar508@gmail.com>
2024-06-28 22:41:45 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -3,6 +3,8 @@ import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //validation
@@ -13,19 +15,28 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   //database call
-  const user = await userModel.findOne({email})
-  if(user){
-    const error = createHttpError("400","User already exist with this email");
-    return next(error)
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError("400", "User already exist with this email");
+    return next(error);
   }
   //password hash
-  const hashedPassword =await bcrypt.hash(password,10);
-  
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
+  //token generation
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
 
   //response
   res.json({
-    message: "user created",
+    accessToken: token,
   });
 };
 
