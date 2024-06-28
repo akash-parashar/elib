dd1dbbb050d45c6914a3f0942360d6d7fa299f71
akash-parashar <aparashar508@gmail.com>
2024-06-28 21:45:52 +0530

    added usermodel

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,20 +1,26 @@
 import exp from "constants";
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  //validation
+  const { name, email, password } = req.body;
 
-const createUser= async(req:Request,res:Response,next:NextFunction)=>{
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "all field are required");
+    return next(error);
+  }
+  //database call
+  const user = await userModel.findOne({email})
+  if(user){
+    const error = createHttpError("400","User already exist with this email");
+    return next(error)
+  }
+  //response
+  res.json({
+    message: "user created",
+  });
+};
 
-    const{ name,email,password}  = req.body;
-
-    if(!name||!email ||!password){
-        const error = createHttpError(400,"all field are required")
-        return next(error)
-    }
-res.json({
-    message:"user created"
-})
-
-}
-
-export {createUser}
+export { createUser };
