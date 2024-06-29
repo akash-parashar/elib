19e43d1ebee3ec57da4cf7b8479cf3cc58d67848
akash-parashar <aparashar508@gmail.com>
2024-06-30 01:43:24 +0530

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,11 +2,15 @@ import { Request, Response, NextFunction } from "express";
 import path from "path";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "node:fs"
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log("files:", req.files);
+  const {title,genre} = req.body;
 
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
 
   const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
 
@@ -41,7 +45,24 @@ try {
   })
 console.log("uploadResult:", uploadResult);
 
-res.json({});
+console.log("bookfile upload:",bookFileUploadResult)
+
+
+const newBook = await bookModel.create({
+  title,
+  genre,
+  author:"667eea027a75a66dacd1e0b5",
+  coverImage: uploadResult.secure_url,
+  file: bookFileUploadResult.secure_url
+  })
+//delete temp files
+
+await fs.promises.unlink(filePath)
+await fs.promises.unlink(bookFilePath)
+
+
+res.status(201).json({id:newBook._id});
+console.log(newBook._id)
 } catch (error) {
   console.log("error:", error);
   return next(createHttpError(500,"error while uploading the files"))
