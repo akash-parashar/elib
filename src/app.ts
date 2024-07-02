03be01b5792811e3991c0f849ac29dde6e04240f
akash-parashar <aparashar508@gmail.com>
2024-07-02 15:41:12 +0530

    add cors supporter

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandlers";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
-
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+
+
+app.use(cors({
+  origin:config.frontendDomain
+}))
 app.use(express.json());
 
 app.get("/", (req, res) => {
